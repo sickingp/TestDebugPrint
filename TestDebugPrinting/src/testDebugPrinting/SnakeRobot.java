899873783b4602d77e2bf1ab77f059c55bd24136
Paul Sicking <sickingp@gmail.com>
2018-02-15 23:44:46 -0800

    Added type to Snake Robot

--- TestDebugPrinting/src/testDebugPrinting/SnakeRobot.java
+++ TestDebugPrinting/src/testDebugPrinting/SnakeRobot.java
@@ -8,18 +8,22 @@ package testDebugPrinting;
  *
  */
 public class SnakeRobot extends Robot {
+	
+	private String m_type;
 
 	/**
 	 * @param name
 	 * @param competitionYear
 	 */
-	public SnakeRobot(String name, int competitionYear) {
+	public SnakeRobot(String name, int competitionYear, String typeOfSnake) {
 		super(name, competitionYear);
+		m_type = typeOfSnake;
+		
 		// TODO Auto-generated constructor stub
 	}
 	
 	public String toString()
 	{
-		return "This is a Snake Robot";
+		return "This is a Snake Robot of type: " + m_type;
 	}
 }
